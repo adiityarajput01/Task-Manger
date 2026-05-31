5831da30778c81293e957f71cc853cc04e2219a6
adiityarajput01 <adityakrbxr363@gmail.com>
2026-05-31 23:04:52 +0000

    Added user authentication with register and login APIS

---
+++ backend/controllers/authController.js
@@ -0,0 +1,79 @@
+const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const generateToken = (id) =>{
+    return jwt.sign(
+    { id },          
+    process.env.JWT_SECRET,     
+    { expiresIn: '7d' } 
+
+);
+};
+
+const register = async (req, res)=> {
+    try{
+    const {name, email, password} = req.body;
+    if(!name || !email || !password) {
+        return res.status(400).json({message: 'All fields are required.'});
+    }
+
+    const existingUser = await User.findOne({ email})
+    if(existingUser) return res.status(400).json({ message: "User already exists"});
+    
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    res.status(201).json({
+        message: "User registered successfully",
+        token: generateToken(user._id),
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        }
+    });
+} catch (err){
+    res.status(500).json({message: "Server error", error: err.message});
+}
+};
+
+const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        if (!email || !password) return res.status(400).json({message: "All feilds are required"});
+
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({message: "Invaild Credentials"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({message: "Invaild Credentials"});
+
+        res.status(200).json({
+            message: "Login successfully",
+            token: generateToken(user._id),
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        });
+
+    }catch (err){
+        res.status(500).json({message: "Server error", error: err.message});
+    }
+};
+
+module.exports = {
+    register,
+    login
+};
+
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require('dotenv').config(); 
+const auth_routes = require('./routes/auth');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -8,6 +9,8 @@ connectDB();
 
 
 app.use(express.json());
+
+app.use("/api/auth", auth_routes)
 
 app.route('/').get((req,res)=>{
     res.send('Server is running.');
