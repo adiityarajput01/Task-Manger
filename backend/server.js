996d87d4a193e724e86f01f5e9a33dd78af90434
adiityarajput01 <adityakrbxr363@gmail.com>
2026-05-31 13:02:28 +0000

    Server is running on PORT

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.route('/').get((req,res)=>{
+    res.send('Server is running.');
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(5000, ()=>{console.log(`Server Started on port {$PORT}`)});
